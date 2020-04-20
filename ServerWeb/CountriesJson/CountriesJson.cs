c1e0583dfaaed2790a9a9bdaf19f445c45e65db2
Ekaterina Kondrashkina <kkondrashkina@outlook.com>
2020-04-20 21:23:11 +0700

    Исправлена задача Страны JSON

---
+++ ServerWeb/CountriesJson/CountriesJson.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CountriesJson
+{
+    class CountriesJson
+    {
+        static void Main(string[] args)
+        {
+            var countries = JsonConvert.DeserializeObject<List<Country>>(File.ReadAllText(".\\countries.txt"));
+
+            var population = 0;
+
+            var allCurrencies = new List<string>();
+
+            foreach (var country in countries)
+            {
+                population += country.Population;
+
+                allCurrencies.AddRange(country.Currencies.Select(currency => currency.Name));
+            }
+
+            Console.WriteLine("Суммарное население:");
+            Console.WriteLine(population);
+
+            Console.WriteLine();
+
+            var uniqueCurrencies = allCurrencies.Distinct();
+
+            Console.WriteLine("Все валюты:");
+
+            foreach (var currency in uniqueCurrencies)
+            {
+                Console.WriteLine(currency);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ServerWeb/CountriesJson/RegionalBlocs.cs
@@ -0,0 +1,10 @@
+﻿namespace CountriesJson
+{
+    public class RegionalBlocs
+    {
+        public string Acronym { get; set; }
+        public string Name { get; set; }
+        public string[] OtherAcronyms { get; set; }
+        public string[] OtherNames { get; set; }
+    }
+}
