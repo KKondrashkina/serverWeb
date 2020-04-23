728af6997aa038b2b1bc3c746be65ff4cdf08366
Ekaterina Kondrashkina <kkondrashkina@outlook.com>
2020-04-23 19:08:36 +0700

    Добавлен проект транзакции

---
+++ ServerWeb/Transactions/Transactions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Transactions
+{
+    class Transactions
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var transaction = connection.BeginTransaction();
+                try
+                {
+                    var sql = "INSERT INTO Category(Name) VALUES(N'Goods for pets')";
+
+                    var command = new SqlCommand(sql, connection)
+                    {
+                        Transaction = transaction
+                    };
+
+                    command.ExecuteNonQuery();
+
+                    throw new Exception("Error");
+
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                }
+            }
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var sql = "INSERT INTO Category(Name) VALUES(N'Goods for pets')";
+                var command = new SqlCommand(sql, connection);
+                command.ExecuteNonQuery();
+
+                throw new Exception("Error");
+            }
+        }
+    }
+}
