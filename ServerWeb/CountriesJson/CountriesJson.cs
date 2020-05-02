8bfa7a56a77c8ff89a30bd64ab92942e2f62bde9
Ekaterina Kondrashkina <kkondrashkina@outlook.com>
2020-05-02 15:27:34 +0700

    Исправлены замечания в CountriesJson

--- ServerWeb/CountriesJson/CountriesJson.cs
+++ ServerWeb/CountriesJson/CountriesJson.cs
@@ -10,25 +10,22 @@ namespace CountriesJson
     {
         static void Main(string[] args)
         {
-            var countries = JsonConvert.DeserializeObject<List<Country>>(File.ReadAllText(".\\countries.txt"));
+            var countries = JsonConvert.DeserializeObject<List<Country>>(File.ReadAllText("countries.txt"));
 
-            var population = 0;
-
-            var allCurrencies = new List<string>();
-
-            foreach (var country in countries)
-            {
-                population += country.Population;
-
-                allCurrencies.AddRange(country.Currencies.Select(currency => currency.Name));
-            }
+            var population = countries.Sum(c => c.Population);
 
             Console.WriteLine("Суммарное население:");
             Console.WriteLine(population);
 
             Console.WriteLine();
 
-            var uniqueCurrencies = allCurrencies.Distinct();
+            var uniqueCurrencies = countries.SelectMany(c => c.Currencies)
+                .Where(c => !string.IsNullOrEmpty(c.Code))
+                .OrderBy(c => c.Name)
+                .Select(c => c.Name)
+                .Where(c => !string.IsNullOrEmpty(c))
+                .Distinct()
+                .ToList();
 
             Console.WriteLine("Все валюты:");
 
